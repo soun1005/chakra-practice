0ed016b187028fd158a86739f09068dcc795e189
Soeun Lee <thdms1005@gmail.com>
2024-04-01 13:50:49 +0200

    course4 grid

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,23 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
     return (
-        <div>
-            <Navbar />
-            <Outlet />
-        </div>
+        <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+            <GridItem
+                as="aside"
+                colSpan="1"
+                bg="pink.400"
+                minHeight="100vh"
+                p="30px"
+            >
+                <span>Sidebar</span>
+            </GridItem>
+            <GridItem as="main" colSpan="5" p="40px">
+                <Navbar />
+                <Outlet />
+            </GridItem>
+        </Grid>
     );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,42 +1,58 @@
-import { Heading, Text, Container, Box } from "@chakra-ui/react";
+import { Heading, Text, Container, Box, SimpleGrid } from "@chakra-ui/react";
 
 export default function Dashboard() {
-    const boxStyles = {
-        ":hover": {
-            color: "pink",
-            fontWeight: "600",
-        },
-    };
+    // const boxStyles = {
+    //     ":hover": {
+    //         color: "pink",
+    //         fontWeight: "600",
+    //     },
+    // };
 
     return (
-        <Container as="section">
-            <Heading my="30px" p="10px">
-                Chakra UI
-            </Heading>
-            <Text ml="30px">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-            </Text>
-            <Text ml="30px" color="pink.500" fontWeight="bold">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-            </Text>
+        <SimpleGrid p="10" columns={4} spacing={10} minChildWidth="250px">
+            <Box bg="white" h="200px" border="1px solid"></Box>
+            <Box bg="white" h="200px" border="1px solid"></Box>
+            <Box bg="white" h="200px" border="1px solid"></Box>
+            <Box bg="white" h="200px" border="1px solid"></Box>
 
-            <Box my="30px" p="20px" bg="orange">
-                <Text color="white">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
-                </Text>
-            </Box>
+            <Box bg="white" h="200px" border="1px solid"></Box>
+            <Box bg="white" h="200px" border="1px solid"></Box>
+            <Box bg="white" h="200px" border="1px solid"></Box>
+            <Box bg="white" h="200px" border="1px solid"></Box>
 
-            <Box sx={boxStyles}>Hello</Box>
-        </Container>
+            <Box bg="white" h="200px" border="1px solid"></Box>
+            <Box bg="white" h="200px" border="1px solid"></Box>
+            <Box bg="white" h="200px" border="1px solid"></Box>
+            <Box bg="white" h="200px" border="1px solid"></Box>
+        </SimpleGrid>
+        // <Container as="section">
+        //     <Heading my="30px" p="10px">
+        //         Chakra UI
+        //     </Heading>
+        //     <Text ml="30px">
+        //         Lorem Ipsum is simply dummy text of the printing and typesetting
+        //         industry. Lorem Ipsum has been the industry's standard dummy
+        //         text ever since the 1500s, when an unknown printer took a galley
+        //         of type and scrambled it to make a type specimen book.
+        //     </Text>
+        //     <Text ml="30px" color="pink.500" fontWeight="bold">
+        //         Lorem Ipsum is simply dummy text of the printing and typesetting
+        //         industry. Lorem Ipsum has been the industry's standard dummy
+        //         text ever since the 1500s, when an unknown printer took a galley
+        //         of type and scrambled it to make a type specimen book.
+        //     </Text>
+
+        //     <Box my="30px" p="20px" bg="orange">
+        //         <Text color="white">
+        //             Lorem Ipsum is simply dummy text of the printing and
+        //             typesetting industry. Lorem Ipsum has been the industry's
+        //             standard dummy text ever since the 1500s, when an unknown
+        //             printer took a galley of type and scrambled it to make a
+        //             type specimen book.
+        //         </Text>
+        //     </Box>
+
+        //     <Box sx={boxStyles}>Hello</Box>
+        // </Container>
     );
 }
